0a6085fc0bfd9b309086a1bf674cf692a7ba9eff
ncortim <ncortim@proton.me>
2025-02-10 11:36:25 +0100

    add groovy script

---
+++ scripto.groovy
@@ -0,0 +1,15 @@
+def buildApp() {
+
+    echo 'building the application...'
+}
+def testApp() {
+
+    echo 'building the application...'
+}
+def deploydApp() {
+
+    echo 'building the application...'
+    echo "deploying version ${params.VERSION}"
+}
+
+return this
