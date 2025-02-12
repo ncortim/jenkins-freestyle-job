f0025cdf534c494ae25f2153138f661663b2383a
ncortim <ncortim@proton.me>
2025-02-12 09:42:25 +0100

    create example for multibranch job and modify used jenkinfiles

---
+++ scripto-simple.groovy
@@ -0,0 +1,14 @@
+def testApp() {
+    echo 'building the application...'
+    echo "executing pipeline for branch ${BRANCH_NAME}"
+}
+
+def buildApp() {
+    echo 'building the application...'
+}
+
+def deployApp() {
+    echo 'building the application...'
+}
+
+return this
